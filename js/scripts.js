1983b9a24d81da5a6c0a5b64fcc637a3c6fbe1f0
Creastv <Creastv@gmail.com>
2021-10-15 09:55:46 +0200

    Optymalizacja
    Added lazyload + .mi.css

--- js/scripts.js
+++ js/scripts.js
@@ -2,108 +2,110 @@
 AOS.init();
 
 ///////////////////////////////////////////// hamburger
-var el = document.querySelector('.hamburger');
-var nav = document.querySelector('#header nav');
-el.onclick = function() {
-  el.classList.toggle('is-active');
-  nav.classList.toggle('open');
-}
-var clo = document.querySelectorAll('#header nav a');
-  clo.forEach(function(a) {
-    a.onclick = function() {
-      nav.classList.remove('open');
-      el.classList.remove('is-active');
-    }
-  });
+var el = document.querySelector(".hamburger");
+var nav = document.querySelector("#header nav");
+el.onclick = function () {
+  el.classList.toggle("is-active");
+  nav.classList.toggle("open");
+};
+var clo = document.querySelectorAll("#header nav a");
+clo.forEach(function (a) {
+  a.onclick = function () {
+    nav.classList.remove("open");
+    el.classList.remove("is-active");
+  };
+});
 ///////////////////////////////////////////// Oferta
-var tog = document.querySelector('.toggle p');
-var tab = document.querySelector('.nav-tabs');
-tog.onclick = function() {
+var tog = document.querySelector(".toggle p");
+var tab = document.querySelector(".nav-tabs");
+tog.onclick = function () {
   // el.classList.toggle('is-active');
-  tab.classList.toggle('openTab');
-}
-var tabHref = document.querySelectorAll('.nav-tabs li a');
-  tabHref.forEach(function(a) {
-    a.onclick = function() {
-      tab.classList.remove('openTab');
-    }
-  });
+  tab.classList.toggle("openTab");
+};
+var tabHref = document.querySelectorAll(".nav-tabs li a");
+tabHref.forEach(function (a) {
+  a.onclick = function () {
+    tab.classList.remove("openTab");
+  };
+});
 ///////////////////////////////////////////// Slider
 var swiper = new Swiper(".slider-home", {
-    loop:true,
-    effect: "fade",
-    speed: 1000,
-    autoplay: {
-      enabled: true,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+  loop: true,
+  // effect: "fade",
+  speed: 1000,
+  autoplay: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 /////////////////////////////////////////////// scroll to top
-(function() {
-  'use strict';
+(function () {
+  "use strict";
   function trackScroll() {
     var scrolled = window.pageYOffset;
     var coords = document.documentElement.clientHeight;
 
     if (scrolled > coords) {
-      goTopBtn.classList.add('upDisplay');
+      goTopBtn.classList.add("upDisplay");
     }
     if (scrolled < coords) {
-      goTopBtn.classList.remove('upDisplay');
+      goTopBtn.classList.remove("upDisplay");
     }
   }
-  var goTopBtn = document.querySelector('.goToTop');
-  window.addEventListener('scroll', trackScroll);
+  var goTopBtn = document.querySelector(".goToTop");
+  window.addEventListener("scroll", trackScroll);
 })();
 ///////////////////////////////////////////// scroll spy adding class to nav elements
-(function() {
-  'use strict';
+(function () {
+  "use strict";
   var section = document.querySelectorAll(".section");
   var sections = {};
   var i = 0;
-  Array.prototype.forEach.call(section, function(e) {
+  Array.prototype.forEach.call(section, function (e) {
     sections[e.id] = e.offsetTop - 150;
   });
-  window.onscroll = function() {
-    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+  window.onscroll = function () {
+    var scrollPosition =
+      document.documentElement.scrollTop || document.body.scrollTop;
 
     for (i in sections) {
       if (sections[i] <= scrollPosition) {
-        document.querySelector('.active').setAttribute('class', ' ');
-        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+        document.querySelector(".active").setAttribute("class", " ");
+        document
+          .querySelector("a[href*=" + i + "]")
+          .setAttribute("class", "active");
       }
     }
   };
 })();
 
 // Oferta tabs
-window.addEventListener("load", function() {
-	// store tabs variable
-	var myTabs = document.querySelectorAll("ul.nav-tabs > li");
+window.addEventListener("load", function () {
+  // store tabs variable
+  var myTabs = document.querySelectorAll("ul.nav-tabs > li");
   function myTabClicks(tabClickEvent) {
-		for (var i = 0; i < myTabs.length; i++) {
-			myTabs[i].classList.remove("active");
-		}
-		var clickedTab = tabClickEvent.currentTarget;
-		clickedTab.classList.add("active");
-		tabClickEvent.preventDefault();
-		var myContentPanes = document.querySelectorAll(".tab-pane");
-		for (i = 0; i < myContentPanes.length; i++) {
-			myContentPanes[i].classList.remove("active");
-		}
-		var anchorReference = tabClickEvent.target;
-		var activePaneId = anchorReference.getAttribute("href");
-		var activePane = document.querySelector(activePaneId);
-		activePane.classList.add("active");
-	}
-	for (i = 0; i < myTabs.length; i++) {
-		myTabs[i].addEventListener("click", myTabClicks)
-	}
+    for (var i = 0; i < myTabs.length; i++) {
+      myTabs[i].classList.remove("active");
+    }
+    var clickedTab = tabClickEvent.currentTarget;
+    clickedTab.classList.add("active");
+    tabClickEvent.preventDefault();
+    var myContentPanes = document.querySelectorAll(".tab-pane");
+    for (i = 0; i < myContentPanes.length; i++) {
+      myContentPanes[i].classList.remove("active");
+    }
+    var anchorReference = tabClickEvent.target;
+    var activePaneId = anchorReference.getAttribute("href");
+    var activePane = document.querySelector(activePaneId);
+    activePane.classList.add("active");
+  }
+  for (i = 0; i < myTabs.length; i++) {
+    myTabs[i].addEventListener("click", myTabClicks);
+  }
 });
-
 
 /*///////////////////////////////////////////// Slider and lightbox gallery
 part one - Swiper initialization
@@ -120,7 +122,7 @@ var mySwiper = new Swiper(".gallery", {
   slideToClickedSlide: false,
   autoplay: {
     delay: 3000,
-    disableOnInteraction: false 
+    disableOnInteraction: false,
   },
   // If we need pagination
   pagination: {
@@ -146,15 +148,15 @@ var mySwiper = new Swiper(".gallery", {
 
 // 2 of 4 : PHOTOSWIPE #######################################
 
-var initPhotoSwipeFromDOM = function(gallerySelector) {
-  var parseThumbnailElements = function(el) {
+var initPhotoSwipeFromDOM = function (gallerySelector) {
+  var parseThumbnailElements = function (el) {
     var thumbElements = el.childNodes,
-        numNodes = thumbElements.length,
-        items = [],
-        figureEl,
-        linkEl,
-        size,
-        item;
+      numNodes = thumbElements.length,
+      items = [],
+      figureEl,
+      linkEl,
+      size,
+      item;
 
     for (var i = 0; i < numNodes; i++) {
       figureEl = thumbElements[i]; // <figure> element
@@ -172,7 +174,7 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
       item = {
         src: linkEl.getAttribute("href"),
         w: parseInt(size[0], 10),
-        h: parseInt(size[1], 10)
+        h: parseInt(size[1], 10),
       };
 
       if (figureEl.children.length > 1) {
@@ -198,14 +200,14 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
   };
 
   // triggers when user clicks on thumbnail
-  var onThumbnailsClick = function(e) {
+  var onThumbnailsClick = function (e) {
     e = e || window.event;
     e.preventDefault ? e.preventDefault() : (e.returnValue = false);
 
     var eTarget = e.target || e.srcElement;
 
     // find root element of slide
-    var clickedListItem = closest(eTarget, function(el) {
+    var clickedListItem = closest(eTarget, function (el) {
       return el.tagName && el.tagName.toUpperCase() === "LI";
     });
 
@@ -213,10 +215,10 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
       return;
     }
     var clickedGallery = clickedListItem.parentNode,
-        childNodes = clickedListItem.parentNode.childNodes,
-        numChildNodes = childNodes.length,
-        nodeIndex = 0,
-        index;
+      childNodes = clickedListItem.parentNode.childNodes,
+      numChildNodes = childNodes.length,
+      nodeIndex = 0,
+      index;
 
     for (var i = 0; i < numChildNodes; i++) {
       if (childNodes[i].nodeType !== 1) {
@@ -235,9 +237,9 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
     }
     return false;
   };
-  var photoswipeParseHash = function() {
+  var photoswipeParseHash = function () {
     var hash = window.location.hash.substring(1),
-        params = {};
+      params = {};
 
     if (hash.length < 5) {
       return params;
@@ -262,19 +264,19 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
     return params;
   };
 
-  var openPhotoSwipe = function(
-  index,
-   galleryElement,
-   disableAnimation,
-   fromURL
+  var openPhotoSwipe = function (
+    index,
+    galleryElement,
+    disableAnimation,
+    fromURL
   ) {
     var pswpElement = document.querySelectorAll(".pswp")[0],
-        gallery,
-        options,
-        items;
+      gallery,
+      options,
+      items;
 
     items = parseThumbnailElements(galleryElement);
-    // #################### 3/4 define photoswipe options (if needed) #################### 
+    // #################### 3/4 define photoswipe options (if needed) ####################
     options = {
       closeEl: true,
       captionEl: true,
@@ -285,19 +287,18 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
       arrowEl: true,
       preloaderEl: true,
       galleryUID: galleryElement.getAttribute("data-pswp-uid"),
-      getThumbBoundsFn: function(index) {
+      getThumbBoundsFn: function (index) {
         var thumbnail = items[index].el.getElementsByTagName("img")[0],
-            pageYScroll =
+          pageYScroll =
             window.pageYOffset || document.documentElement.scrollTop,
-            rect = thumbnail.getBoundingClientRect();
+          rect = thumbnail.getBoundingClientRect();
 
         return { x: rect.left, y: rect.top + pageYScroll, w: rect.width };
-      }
+      },
     };
 
     if (fromURL) {
       if (options.galleryPIDs) {
-    
         for (var j = 0; j < items.length; j++) {
           if (items[j].pid == index) {
             options.index = j;
@@ -326,13 +327,13 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
     gallery.init();
 
     /* ########### PART 4 - EXTRA CODE  ########### */
-    gallery.listen("unbindEvents", function() {
+    gallery.listen("unbindEvents", function () {
       let getCurrentIndex = gallery.getCurrentIndex();
       mySwiper.slideTo(getCurrentIndex, 0, false);
       mySwiper.autoplay.start();
     });
-    gallery.listen('initialZoomIn', function() {
-      if(mySwiper.autoplay.running){
+    gallery.listen("initialZoomIn", function () {
+      if (mySwiper.autoplay.running) {
         mySwiper.autoplay.stop();
       }
     });
@@ -351,19 +352,60 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 initPhotoSwipeFromDOM(".my-gallery");
 
 // map
- function initMap() {
-        var uluru = {lat: 50.222168, lng: 19.0670185};
-        var map = new google.maps.Map(document.getElementById('mapa-it'), {
-          zoom: 15,
-          center: {lat: 50.2231978, lng: 19.0568391},
-          
-          styles:[{"featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{"color": "#444444"} ] }, {"featureType": "landscape", "elementType": "all", "stylers": [{"color": "#f2f2f2"} ] }, {"featureType": "poi", "elementType": "all", "stylers": [{"visibility": "off"} ] }, {"featureType": "road", "elementType": "all", "stylers": [{"saturation": -100 }, {"lightness": 45 } ] }, {"featureType": "road.highway", "elementType": "all", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "road.arterial", "elementType": "labels.icon", "stylers": [{"visibility": "off"} ] }, {"featureType": "transit", "elementType": "all", "stylers": [{"visibility": "off"} ] }, {"featureType": "water", "elementType": "all", "stylers": [{"color": "#ee9e9e"}, {"visibility": "on"} ] } ]
-        });
+function initMap() {
+  var uluru = { lat: 50.222168, lng: 19.0670185 };
+  var map = new google.maps.Map(document.getElementById("mapa-it"), {
+    zoom: 15,
+    center: { lat: 50.2231978, lng: 19.0568391 },
 
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map,
-          title: 'Restauracja Dworek „Pod Lipami”',
-          icon:'img/pin.png',
-        });
-      }
+    styles: [
+      {
+        featureType: "administrative",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#444444" }],
+      },
+      {
+        featureType: "landscape",
+        elementType: "all",
+        stylers: [{ color: "#f2f2f2" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "all",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "road",
+        elementType: "all",
+        stylers: [{ saturation: -100 }, { lightness: 45 }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "all",
+        stylers: [{ visibility: "simplified" }],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "labels.icon",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "all",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "water",
+        elementType: "all",
+        stylers: [{ color: "#ee9e9e" }, { visibility: "on" }],
+      },
+    ],
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    title: "Restauracja Dworek „Pod Lipami”",
+    icon: "img/pin.png",
+  });
+}
